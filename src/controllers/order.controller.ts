6466b65607d6fd2fdc7a94978bfcca5a05b2e8fe
Nycolas Messias <nycolas.messias@sossego.com.br>
2022-02-27 19:57:58 -0300

    add param validation and error handling at order controller

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,29 +1,45 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { orderService } from '../services/order.service';
 
-async function getOrderSummary(req: Request, res: Response, next: NextFunction): Promise<void> {
+async function getOrderSummary(req: Request, res: Response): Promise<void> {
     try {
         const pastDays = parseInt(req.params.pastDays) || 5;
+
+        if (pastDays < 0) {
+            throw new Error('Invalid params!');
+        }
 
         const orderSummary = await orderService.getOrderSummary(pastDays);
 
         res.status(200).send(orderSummary);
     } catch (error) {
-        // <Update status depending on error and respond>
-        res.end();
+        if (error instanceof Error) {
+            error.message.includes('params') ? res.status(400) : res.status(500);
+            res.send(error.message);
+        } else {
+            res.status(500).end();
+        }
     }
 }
 
-async function refreshOrderSummaries(req: Request, res: Response, next: NextFunction): Promise<void> {
+async function refreshOrderSummaries(req: Request, res: Response): Promise<void> {
     try {
         const pastDays = parseInt(req.body.pastDays) || 1;
+
+        if (pastDays < 0) {
+            throw new Error('Invalid params!');
+        }
 
         orderService.refreshOrderSummaries(pastDays);
 
         res.status(200).send(`Job successfully created. Refreshing order summaries for the past ${pastDays} days.`);
     } catch (error) {
-        // <Update status depending on error and respond>
-        res.end()
+        if (error instanceof Error) {
+            error.message.includes('params') ? res.status(400) : res.status(500);
+            res.send(error.message);
+        } else {
+            res.status(500).end();
+        }
     }
 }
 
